f309ca2b44a8da0d4be3303870dc19da355628ae
maxkram <max.kramer@live.com>
2018-02-26 12:44:28 -0500

    ok got rid off all the warnings and errors

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 
 class Contact extends Component {
   render() {
+    if(this.props.data){
+      var name = this.props.data.name;
+      var street = this.props.data.address.street;
+      var city = this.props.data.address.city;
+      var state = this.props.data.address.state;
+      var zip = this.props.data.address.zip;
+      var phone = this.props.data.phone;
+      var email = this.props.data.email;
+    }
     return (
       <section id="contact">
          <div className="row section-head">
@@ -68,10 +77,11 @@ class Contact extends Component {
 
 					   <h4>Address and Phone</h4>
 					   <p className="address">
-						   Jonathan Doe<br />
-						   1600 Amphitheatre Parkway <br />
-						   Mountain View, CA 94043 US<br />
-						   <span>(123) 456-7890</span>
+						   {name}<br />
+						   {street} <br />
+						   {city}, {state} {zip}<br />
+						   <span>{phone}</span><br />
+               <span>{email}</span>
 					   </p>
 
 				   </div>
